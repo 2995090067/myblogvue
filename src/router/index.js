d1c6771e43341700c3200a72b129b5b09defb548
2995090067 <2995090067@qq.com>
2020-07-14 16:34:38 +0800

    'chushi\'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+let About=()=>import('../views/About');
+let Home=()=>import('../views/Home');
 
 Vue.use(VueRouter)
 
@@ -13,9 +15,7 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
