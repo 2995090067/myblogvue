178452abdf34cb9f5315713888bb92b81b29198c
2995090067 <2995090067@qq.com>
2020-07-16 23:20:48 +0800

    'login-sin'

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
     <el-container>
-      <el-header>我是标题</el-header>
+      <el-header>
+          <img class="logo" src="https://exp.bdstatic.com/static/exp-pc/common-jquery/widget/search-box/img/logo_83ae7e2.png" >
+      </el-header>
       <el-main>
-        <h1>weclome login</h1>
-        我是内容
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+<!--          用户名-->
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+          </el-form-item>
+<!--          密码-->
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="ruleForm.password"></el-input>
+          </el-form-item>
+<!--提交-->
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
       </el-main>
     </el-container>
   </div>
@@ -12,10 +27,99 @@
 
 <script>
   export default {
-    name: "Login"
+    name: "Login",
+    data() {
+      return {
+        ruleForm: {
+          username: '',
+          password: ''
+        },
+        rules: {
+          username: [
+            {required: true, message: '请输入用户名', trigger: 'blur'},
+            {min: 3, max: 7, message: '长度在 3 到 7 个字符', trigger: 'blur'}
+          ],
+          password: [
+            {require: true, message:'请输入密码', trigger: 'change'},
+            {min: 7, max: 11, message: '长度在7到11位', trigger: 'change'}
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style>
+
+  /*页面样式*/
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    /*background-color: #E9EEF3;*/
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+  .mlogo {
+    height: 60%;
+    margin-top: 10px;
+  }
+
+  .demo-ruleForm {
+    max-width: 500px;
+    margin: 0 auto;
+  }
+
+  /*图片样式*/
+
+  .logo{
+    /*width: 20%;*/
+    margin-top: 6px;
+    margin-bottom: 6px;
+    height: 82%;
+
+  }
 
 </style>
